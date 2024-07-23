97938c2e9210644ecc64d029ce51e3749452ab27
Lionseun Ling <lionseun@gmail.com>
2024-07-23 23:34:13 +0800

    优化代码

--- src/main.rs
+++ src/main.rs
@@ -45,7 +45,6 @@ mod proxy;
 mod tls;
 mod utils;
 
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info"))
---
+++ src/tls.rs
@@ -0,0 +1,4 @@
+pub fn build_cert(c: &[u8]) -> std::io::Result<Vec<Vec<u8>>> {
+    todo!("not impl")
+}
+
